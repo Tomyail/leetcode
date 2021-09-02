9e0a991afe97b491e33fb4be30755b6088690ac9
xuexin.li <xuexin.li@alo7.com>
2021-09-02 13:53:46 +0800

    finish 11

---
+++ 11.盛最多水的容器.test.ts
@@ -0,0 +1,10 @@
+const fn = require('./11.盛最多水的容器');
+it('盛最多水的容器', () => {
+  expect(fn([1, 1])).toBe(1);
+  expect(fn([1, 8, 6, 2, 5, 4, 8, 3, 7])).toBe(49);
+  expect(fn([4, 3, 2, 1, 4])).toBe(16);
+  expect(fn([1, 2, 4, 3])).toBe(4);
+  expect(fn([1, 2, 1])).toBe(2);
+  expect(fn([2,3,4,5,18,17,6])).toBe(17);
+});
+
--- 11.盛最多水的容器.ts
+++ 11.盛最多水的容器.ts
@@ -5,25 +5,52 @@
  */
 
 // @lc code=start
+// function maxArea(height: number[]): number {
+//   //找出 数组里面数值 x 数值的索引差值的乘积最大的两个数字
+
+//   let max = 0;
+//   //先枚举 X 轴,再枚举 Y 轴
+//   for (let xDelta = 1; xDelta < height.length; xDelta++) {
+//     for (let i = 0; i < height.length - xDelta; i++) {
+//       const currentValue =
+//         xDelta * //Math.min(height[i], height[i + xDelta]);
+//         (height[i] < height[i + xDelta] ? height[i] : height[i + xDelta]);
+//       if (currentValue > max) {
+//         max = currentValue;
+//       }
+//     }
+//   }
+
+//   return max;
+// }
+
 function maxArea(height: number[]): number {
-  //找出 数组里面数值 x 数值的索引差值的乘积最大的两个数字
+  let start = 0,
+    end = height.length - 1;
 
   let max = 0;
-  //先枚举 X 轴,再枚举 Y 轴
-  for (let xDelta = 1; xDelta < height.length; xDelta++) {
-    for (let i = 0; i < height.length; i++) {
-      if (i + xDelta > height.length) {
-        break;
-      }
-      const currentValue = xDelta * Math.min(height[i], height[i + xDelta]);
-      if (currentValue > max) {
-        max = currentValue;
-      }
+
+  while (start < end) {
+    //比较两边,面积需要按照短的那一边算
+    if (height[start] > height[end]) {
+      max = Math.max(max, (end - start) * height[end]);
+      //向内移动边,两边都可以移动,但是移动长边后,有2种可能:
+      // 1 有更高的长边
+      // 2 没有更高的长边
+      //无论是哪一种,面积都按照短的那一边算,所以这两个结果都会导致更小的面积.
+      //反过来如果移动短边,也有两种可能
+      // 1 有更高的短边
+      // 2 没有更高的短边
+      // 如果出现情况 1, 获得面积将会大于当前值.
+      // 所以移动短边是唯一能够获得更大面积的方法
+      end--;
+    } else {
+      max = Math.max(max, (end - start) * height[start]);
+      start++;
     }
   }
-
   return max;
 }
 
-module.exports = maxArea
+module.exports = maxArea;
 // @lc code=end
