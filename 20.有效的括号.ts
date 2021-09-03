2960136b3b909b3eb3567124a755d6f142c20bed
xuexin.li <xuexin.li@alo7.com>
2021-09-03 14:10:02 +0800

    finish 20

---
+++ 20.有效的括号.ts
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=typescript
+ *
+ * [20] 有效的括号
+ */
+
+// @lc code=start
+function isValid(s: string): boolean {
+  const pair = [
+    ['(', ')'],
+    ['[', ']'],
+    ['{', '}'],
+  ];
+
+  const open = pair.map((p) => p[0]);
+  const close = pair.map((p) => p[1]);
+
+  const stack = [];
+  for (let i = 0; i < s.length; i++) {
+    const char = s.charAt(i);
+    if (open.includes(char)) {
+      stack.push(char);
+    } else {
+      const target = stack.pop();
+      if (target) {
+        if (open.indexOf(target) !== close.indexOf(char)) {
+          return false;
+        }
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length ? false : true;
+}
+module.exports = isValid;
+// @lc code=end
